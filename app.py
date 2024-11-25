7fd0b1489b446bf39975740d21c38b58d7bdff94
DenisDragos1 <dragosdenisdanut@yahoo.com>
2024-11-25 21:26:27 +0200

    os

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return "Hello, Flask is running in the cloud!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # PORT din mediu sau fallback la 5000
