5982693a82711952c09eb3f7f600e69b28e8e597
DenisDragos1 <dragosdenisdanut@yahoo.com>
2024-11-25 21:18:46 +0200

    update

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def home():
     return "Hello, Flask!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # PORT din mediu sau fallback la 5000
+    app.run(host="0.0.0.0", port=port)
